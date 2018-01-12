83c5121bbe2b12da3b640dfd4cf3d651e9bc77c0
Romain <romain@ex-alto.com>
2018-01-12 13:21:06 +0100

    Nouvelle page d'accueil

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,49 +1,20 @@
 @extends('layouts.frontend.app')
 
 @section('content')
-    <!-- start slider section -->
-    <section class="no-padding full-screen">
-        <div class="swiper-container swiper-vertical-pagination swiper-container-vertical">
-            <div class="swiper-wrapper">
-                <!-- start slide item -->
-                <div class="swiper-slide cover-background" style="background-image: url('{{ asset('images/fond-1.jpg') }}')">
-                    <div class="full-screen width-100 position-relative">
-                        <div class="slider-typography text-left">
-                            <div class="slider-text-middle-main">
-                                <div class="slider-text-middle padding-100px-lr xs-padding-30px-lr">
-                                    <div class="swiper-bottom-content bg-black-opacity-light width-500px padding-80px-all sm-no-margin-left xs-width-90 xs-padding-20px-all ">
-                                        <h4 class="xs-margin-5px-bottom"><a href="single-project-page-01.html" class="font-weight-500 text-white alt-font text-white-hover">Romain Sola Photographies</a></h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- end slide item -->
-                <!-- start slide item -->
-                <div class="swiper-slide cover-background" style="background-image: url('{{ asset('images/fond-2.jpg') }}')">
-                    <div class="full-screen width-100 position-relative">
-                        <div class="slider-typography text-left">
-                            <div class="slider-text-middle-main">
-                                <div class="slider-text-middle padding-100px-lr xs-padding-30px-lr">
-                                    <div class="swiper-bottom-content bg-black-opacity-light width-500px padding-80px-all sm-no-margin-left xs-width-90 xs-padding-20px-all ">
-                                        <h4 class="xs-margin-5px-bottom"><a href="single-project-page-01.html" class="font-weight-500 text-white alt-font text-white-hover">Lisa's Schweizer Branding</a></h4>
-                                        <div class="box-arrow"><a href="single-project-page-01.html"><img width="60" src="{{ asset('images/homepage-option19-arrow-bg.jpg') }}" alt=""></a></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- end slide item -->
 
+<!-- start page title -->
+<section id="home" class="no-padding parallax wow fadeIn position-relative" data-stellar-background-ratio="0.5" style="background-image:url({{ asset('images/fond-3.jpg') }});">
+    <div class="opacity-full bg-extra-dark-gray"></div>
+    <div class="container position-relative one-fourth-screen">
+        <div class="slider-typography text-center">
+            <div class="slider-text-middle-main">
+                <div class="slider-text-middle">
+<img src="{{ asset('images/logo-rs.png') }}">
+                </div>
             </div>
-            <!-- start slider pagination -->
-            <div class="swiper-pagination swiper-pagination-white"></div>
-            <!-- end slider pagination -->
         </div>
-    </section>
-    <!-- end slider section -->
-
+    </div>
+</section>
+<!-- end page title -->
 
 @endsection
--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -39,97 +39,60 @@
 
 <body class="width-100">
 
-<!-- start header -->
-<header>
-    <!-- start navigation -->
-    <nav class="navbar navbar-default bootsnav navbar-top header-dark bg-transparent nav-box-width white-link padding-four-lr menu-logo-center">
-        <div class="container nav-header-container">
-            <div class="row">
-                <!-- start logo -->
-                <div class="center-logo">
-                    <a href="index.html" title="Pofo" class="logo"><img src="images/logo.png" data-at2x="images/logo@2x.png" class="logo-dark" alt="Pofo"><img src="images/logo-white.png" data-at2x="images/logo-white@2x.png" alt="Pofo" class="logo-light default"></a>
-                </div>
-                <!-- end logo -->
-                <div class="col-md-12 col-sm-12 col-xs-12 accordion-menu">
-                    <button type="button" class="navbar-toggle collapsed pull-right" data-toggle="collapse" data-target="#navbar-collapse-toggle-1">
-                        <span class="sr-only">toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <div class="navbar-collapse collapse no-padding-right" id="navbar-collapse-toggle-1">
-                        <ul class="nav navbar-nav navbar-left text-right no-margin alt-font text-normal width-40 sm-width-100 sm-text-left" data-in="fadeIn" data-out="fadeOut">
-                            <!-- start menu item -->
-                            <li class="dropdown megamenu-fw">
-                                <a href="javascript:void(0);">Portfolio</a><i class="fa fa-angle-down dropdown-toggle" data-toggle="dropdown" aria-hidden="true"></i>
-                            </li>
-                        </ul>
-                        <ul class="nav navbar-nav navbar-right no-margin alt-font text-normal width-40 sm-width-100" data-in="fadeIn" data-out="fadeOut">
-                            <!-- start menu item -->
-                            <li class="dropdown megamenu-fw">
-                                <a href="javascript:void(0);">Contact</a><i class="fa fa-angle-down dropdown-toggle" data-toggle="dropdown" aria-hidden="true"></i>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </nav>
-    <!-- end navigation -->
-</header>
+    <!-- start header -->
+    <header>
+
+    </header>
 
 
-@yield('content')
+    @yield('content')
 
-<a class="scroll-top-arrow" href="javascript:void(0);"><i class="ti-arrow-up"></i></a>
-<!-- end scroll to top  -->
-<!-- javascript libraries -->
-<script type="text/javascript" src="{{ asset('js/jquery.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/modernizr.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/jquery.easing.1.3.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/skrollr.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/smooth-scroll.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/jquery.appear.js') }}"></script>
-<!-- menu navigation -->
-<script type="text/javascript" src="{{ asset('js/bootsnav.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/jquery.nav.js') }}"></script>
-<!-- animation -->
-<script type="text/javascript" src="{{ asset('js/wow.min.js') }}"></script>
-<!-- page scroll -->
-<script type="text/javascript" src="{{ asset('js/page-scroll.js') }}"></script>
-<!-- swiper carousel -->
-<script type="text/javascript" src="{{ asset('js/swiper.min.js') }}"></script>
-<!-- counter -->
-<script type="text/javascript" src="{{ asset('js/jquery.count-to.js') }}"></script>
-<!-- parallax -->
-<script type="text/javascript" src="{{ asset('js/jquery.stellar.js') }}"></script>
-<!-- magnific popup -->
-<script type="text/javascript" src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
-<!-- portfolio with shorting tab -->
-<script type="text/javascript" src="{{ asset('js/isotope.pkgd.min.js') }}"></script>
-<!-- images loaded -->
-<script type="text/javascript" src="{{ asset('js/imagesloaded.pkgd.min.js') }}"></script>
-<!-- pull menu -->
-<script type="text/javascript" src="{{ asset('js/classie.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/hamburger-menu.js') }}"></script>
-<!-- counter  -->
-<script type="text/javascript" src="{{ asset('js/counter.js') }}"></script>
-<!-- fit video  -->
-<script type="text/javascript" src="{{ asset('js/jquery.fitvids.js') }}"></script>
-<!-- equalize -->
-<script type="text/javascript" src="{{ asset('js/equalize.min.js') }}"></script>
-<!-- skill bars  -->
-<script type="text/javascript" src="{{ asset('js/skill.bars.jquery.js') }}"></script>
-<!-- justified gallery  -->
-<script type="text/javascript" src="{{ asset('js/justified-gallery.min.js') }}"></script>
-<!--pie chart-->
-<script type="text/javascript" src="{{ asset('js/jquery.easypiechart.min.js') }}"></script>
-<!-- instagram -->
-<script type="text/javascript" src="{{ asset('js/instafeed.min.js') }}"></script>
-<!-- retina -->
-<script type="text/javascript" src="{{ asset('js/retina.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
-
+    <a class="scroll-top-arrow" href="javascript:void(0);"><i class="ti-arrow-up"></i></a>
+    <!-- end scroll to top  -->
+    <!-- javascript libraries -->
+    <script type="text/javascript" src="{{ asset('js/jquery.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/modernizr.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery.easing.1.3.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/skrollr.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/smooth-scroll.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery.appear.js') }}"></script>
+    <!-- menu navigation -->
+    <script type="text/javascript" src="{{ asset('js/bootsnav.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery.nav.js') }}"></script>
+    <!-- animation -->
+    <script type="text/javascript" src="{{ asset('js/wow.min.js') }}"></script>
+    <!-- page scroll -->
+    <script type="text/javascript" src="{{ asset('js/page-scroll.js') }}"></script>
+    <!-- swiper carousel -->
+    <script type="text/javascript" src="{{ asset('js/swiper.min.js') }}"></script>
+    <!-- counter -->
+    <script type="text/javascript" src="{{ asset('js/jquery.count-to.js') }}"></script>
+    <!-- parallax -->
+    <script type="text/javascript" src="{{ asset('js/jquery.stellar.js') }}"></script>
+    <!-- magnific popup -->
+    <script type="text/javascript" src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+    <!-- portfolio with shorting tab -->
+    <script type="text/javascript" src="{{ asset('js/isotope.pkgd.min.js') }}"></script>
+    <!-- images loaded -->
+    <script type="text/javascript" src="{{ asset('js/imagesloaded.pkgd.min.js') }}"></script>
+    <!-- pull menu -->
+    <script type="text/javascript" src="{{ asset('js/classie.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/hamburger-menu.js') }}"></script>
+    <!-- counter  -->
+    <script type="text/javascript" src="{{ asset('js/counter.js') }}"></script>
+    <!-- equalize -->
+    <script type="text/javascript" src="{{ asset('js/equalize.min.js') }}"></script>
+    <!-- skill bars  -->
+    <script type="text/javascript" src="{{ asset('js/skill.bars.jquery.js') }}"></script>
+    <!-- justified gallery  -->
+    <script type="text/javascript" src="{{ asset('js/justified-gallery.min.js') }}"></script>
+    <!--pie chart-->
+    <script type="text/javascript" src="{{ asset('js/jquery.easypiechart.min.js') }}"></script>
+    <!-- instagram -->
+    <script type="text/javascript" src="{{ asset('js/instafeed.min.js') }}"></script>
+    <!-- retina -->
+    <script type="text/javascript" src="{{ asset('js/retina.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
 </body>
 </html>
